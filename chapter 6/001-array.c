52630f702f5e1eeae7cdca4a8c1f9e39fdd162da
LeoPoeX <kingpi@163.com>
2017-09-03 14:23:12 +0800

    new file

---
+++ chapter 6/001-array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+    int i,a[10];
+
+    for (i = 0;i <= 9;i++)
+    {
+        a[i] = i;
+    }
+    for (i = 9;i >= 0;i--)  //output
+    {
+        printf("%d \n",a[i]);
+    }
+}
