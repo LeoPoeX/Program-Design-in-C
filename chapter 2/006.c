243a56d6a6be3b2cbaf59403fdd3ffd27d68ec46
LeoPoeX <kingpi@163.com>
2017-09-12 11:07:20 +0800

    new files

--- chapter 2/006.c
+++ chapter 2/006.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
+#define PI 3.14159
 void main()
 {
-    float PI=3.14159;
-    int s,r=5;
-    s=r*r*PI;
-    printf("s=%d\n",s);
+    int s, r=5;
+    s = r * r * PI;
+    printf("s=%d\n", s);
 }
---
+++ chapter 7/006-pow.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+    double x = 2.0, y = 3.0;
+    double result;
+
+    result = pow(x, y);
+    printf("%f的%f次方是%f \n",x,y,result);
+    return (0);
+}
