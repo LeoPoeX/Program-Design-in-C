e3478b5797b5cd6890ee0713415869104078be86
LeoPoeX <kingpi@163.com>
2017-11-23 18:04:20 +0800

    new files

---
+++ chapter 7/extern/021-extern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+#include"022-extern.c"
+
+int A;
+
+void main()
+{
+    int power(int);
+    int b = 3, c, d, m;
+
+    printf("enter the number A and its power m: \n");
+    scanf("%d %d", &A, &m);
+
+    c = A * b;
+    printf("%d * %d = %d\n", A, b, c);
+
+    d = power(m);
+    printf("%d ^ %d = %d\n", A, m, d);
+
+}
