83c306bb4721e03dd8d5399e7dab9edacd43590a
LeoPoeX <kingpi@163.com>
2017-12-15 16:56:34 +0800

    new file

--- exercise/lb.c
+++ exercise/lb.c
@@ -2,17 +2,21 @@
 #include<malloc.h>
 
 struct student *tj(struct student *head);
+struct student *cr(struct student *head);
 void xs(struct student *head);
+
 struct student  
 {  
     int num;
+    double score;
     struct student *next;
 };
 
 int main()
 {
 	char a;
-	struct student *head = NULL;
+	struct student *head = NULL, *p;
+	p = head;
 
     printf("是否输入(y/n)?");
     scanf("%c", &a);
@@ -20,9 +24,14 @@ int main()
     while(a == 'Y' || a == 'y'){
         head = tj(head);
         xs(head);
+        
+		p = cr(head);
+	    xs(p);
+	    
         printf("是否输入(y/n)?");
         scanf(" %c", &a);
     }
+    
 
     return 0;
 }
@@ -32,7 +41,6 @@ struct student *tj(struct student *head)
 {
 	struct student *p, *p1;
 	p1 = head;
-	int num;
 	
 	p = (struct student *)malloc(sizeof(struct student));
 	if (p == NULL)
@@ -51,14 +59,70 @@ struct student *tj(struct student *head)
 			p1 = p1 -> next;	//p1指向下一个直到结尾 
 		}
 		p1 -> next = p;
-	}
-	printf("请输入数：");
-	scanf("%d", &num);
+	} 
 	
-	p -> num = num;
+	printf("请输入学号：");
+	scanf("%d", &p -> num);
+	
+	printf("请输入成绩：");
+	scanf("%lf", &p -> score);
+
 	p -> next = NULL;
+
+}
+
+	//插入 
+struct student *cr(struct student *head)
+{
+	struct student *p, *p1 = head, *p2;
+	p = (struct student *)malloc(sizeof(struct student));
+	int n;
+	n = p1 ->num;
+	
+	if (p == NULL)
+	{
+		printf("无法读取文件");
+		exit(0);
+	}
+	if (head == NULL)	//如果头指针为空 
+	{
+		head = p;
+	}
+	else
+	{
+		printf("请输入要插入的学号：");
+		scanf("%d", &p ->num);
+		printf("请输入要插入的成绩：");
+		scanf("%lf", &p ->score);
+	
+		while((p1 -> num < p -> num) && (p1 -> next != NULL))
+		{
+			p2 = p1;
+			p1 = p1 -> next;
+		}
+		if(p1 -> num >= p -> num)
+		{
+			if(p1 == head)	//为头节点，新建节点插入头节点前 
+			{
+				p -> next = head;
+				head = p;
+			}
+			else	//普通情况 
+			{
+				p2 = p1;
+				p -> next = p1 -> next;	//新节点指向原节点的下一个 
+				p1 -> next = p;	//当前节点指向新节点 
+			}
+		}
+		else	//位于结尾 
+		{
+			p1 -> next = p;
+			p -> next = NULL;
+		}
+	}
 	
 	return head;
+	
 }
 
 	//打印 
@@ -69,7 +133,7 @@ void xs(struct student *head)
 	
 	while(p != NULL)	//如果p不为空，输出 
 	{
-		printf("  %d\t%d\n", j, p->num);
+		printf("  %d\t%d\t%5.2f\n", j, p->num, p->score);
 		p = p -> next;
 		j++;
 	}
